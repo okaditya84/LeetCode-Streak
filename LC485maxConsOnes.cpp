03728e2b79c7059ee274618e58d19d5b56aa1f5b
Aditya Jethani <jethaniaditya7@gmail.com>
2024-03-17 10:49:44 +0530

    max consecutive Ones LC 485

---
+++ LC485maxConsOnes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int max_count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                max_count=max(max_count,count);
+            }
+            else{
+                count=0;
+            }
+        }
+        return max_count;
+    }
+};
