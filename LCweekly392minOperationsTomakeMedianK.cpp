cdd9d00d63d2b86a39f46fe29161175873c27570
Aditya Jethani <jethaniaditya7@gmail.com>
2024-04-07 08:45:34 +0530

    changed it to cpp

--- LCweekly392minOperationsTomakeMedianK.cpp
+++ LCweekly392minOperationsTomakeMedianK.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    public long minOperationsToMakeMedianK(int[] HappyNumbers, int MagicalNumber) {
-        Arrays.sort(HappyNumbers);
+public:
+    long long minOperationsToMakeMedianK(vector<int>& HappyNumbers, int MagicalNumber) {
+        sort(HappyNumbers.begin(), HappyNumbers.end());
         
-        int TotalMagicians = HappyNumbers.length;
+        int TotalMagicians = HappyNumbers.size();
         int MagicianPosition = TotalMagicians / 2;
         
-        long NumberDifference = MagicalNumber - HappyNumbers[MagicianPosition];
-        long OperationCount = 0;
+        long long NumberDifference = MagicalNumber - HappyNumbers[MagicianPosition];
+        long long OperationCount = 0;
         
         for(int MagicianIndex = 0; MagicianIndex < TotalMagicians; MagicianIndex++) {
             if(HappyNumbers[MagicianIndex] < MagicalNumber && MagicianPosition <= MagicianIndex) {
-                OperationCount = OperationCount + (MagicalNumber - HappyNumbers[MagicianIndex]);
+                OperationCount += MagicalNumber - HappyNumbers[MagicianIndex];
             }
             else if(HappyNumbers[MagicianIndex] > MagicalNumber && MagicianPosition >= MagicianIndex) {
-                OperationCount = OperationCount + (HappyNumbers[MagicianIndex] - MagicalNumber);
+                OperationCount += HappyNumbers[MagicianIndex] - MagicalNumber;
             }
         }
         
         return OperationCount;
     }
-}
+};
